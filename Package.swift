95d6c2030de5a168ce05f998a8753c82481e0485
Michael Rockhold <michael@appel-rockhold.org>
2019-08-26 16:41:40 -0700

    rename Source back to Sources

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "bms-clientsdk-swift-analytics",
-            targets: ["bms-clientsdk-swift-analytics"]),
+            name: "BMSAnalytics",
+            targets: ["BMSAnalytics"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "bms-clientsdk-swift-analytics",
+            name: "BMSAnalytics",
             dependencies: []),
         .testTarget(
-            name: "bms-clientsdk-swift-analyticsTests",
-            dependencies: ["bms-clientsdk-swift-analytics"]),
+            name: "BMSAnalyticsTests",
+            dependencies: ["BMSAnalytics"]),
     ]
 )
